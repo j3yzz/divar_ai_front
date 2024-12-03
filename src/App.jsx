acba714c6c7481be9626418a3c80ab10e6e23fb4
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2024-12-03 12:19:58 +0330

    Add filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,65 @@
-import { useState } from "react";
-import { Button, Textarea, CheckboxGroup } from "@nextui-org/react";
+import { useMemo, useState } from "react";
+import { motion, AnimatePresence } from "motion/react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  Button,
+  Textarea,
+  Divider,
+} from "@nextui-org/react";
 
-import Tag from "@/components/Tag";
+import BuyFilters from "@/components/BuyFilters";
+import RentFilters from "@/components/RentFilters";
+import FiltersIcon from "@/assets/filters.svg";
+
+const buttonVariants = {
+  visible: {
+    y: 0,
+    scale: 1,
+    opacity: 1,
+    transition: { duration: 0.085, ease: [0.4, 0, 0.2, 1] },
+  },
+  hidden: { scale: 0.98, opacity: 0, y: 10 },
+};
 
 function App() {
-  const [groupSelected, setGroupSelected] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [category, setCategory] = useState("");
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  const categoryName = useMemo(() => {
+    switch (category) {
+      case "buy":
+        return "خرید";
+
+      case "rent":
+        return "رهن و اجاره";
+
+      default:
+        return "";
+    }
+  }, [category]);
+
+  const onModalClose = () => {
+    setCategory("");
+    onOpenChange();
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
   return (
-    <main className="min-h-screen flex justify-center items-center">
+    <motion.main
+      transition={{ duration: 0.3 }}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      className="min-h-screen flex justify-center items-center"
+    >
       <div className="max-w-4xl mx-auto text">
         <section className="text-center">
           <h1 className="text-5xl font-extrabold leading-snug bg-gradient-to-r from-purple-500 via-pink-500 to-blue-500 text-transparent bg-clip-text">
@@ -23,69 +75,124 @@ function App() {
           <Textarea
             placeholder="هر چی دل تنگت می‌خواد سرچ کن..."
             minRows={6}
+            value={inputValue}
+            onChange={handleChange}
             classNames={{
               inputWrapper:
                 "bg-gray-100 data-[hover=true]:bg-gray-100 group-data-[focus=true]:bg-gray-100 p-4",
               input:
-                "group-data-[has-value=true]:text-slate-500 text-xl font-bold placeholder:text-slate-400",
+                "group-data-[has-value=true]:text-slate-500 text-xl font-bold placeholder:text-slate-400 max-w-[83%]",
             }}
           />
-          <Button
-            className="absolute left-4 bottom-4"
-            color="primary"
-            radius="sm"
-            size="lg"
-          >
-            شروع جستجو
-          </Button>
-        </div>
 
-        <div className="flex gap-2">
-          <CheckboxGroup
-            className="gap-1"
-            orientation="horizontal"
-            value={groupSelected}
-            onChange={setGroupSelected}
+          <div
+            onClick={onOpen}
+            className="absolute left-4 top-4 w-8 h-8 flex items-center justify-center bg-slate-950 rounded-md cursor-pointer"
           >
-            <Tag value="wifi">تگ خونه</Tag>
-            <Tag value="tv">تگ آسانسور</Tag>
-            <Tag value="kitchen">تگ پارکینگ</Tag>
-            <Tag value="parking">تگ تراس</Tag>
-          </CheckboxGroup>
-          {/* <Chip
-            size="lg"
-            variant="shadow"
-            color="default"
-            // classNames={{
-            //   base: "bg-gradient-to-br from-indigo-500 to-pink-500  border-white/50 shadow-pink-500/30",
-            //   content: "drop-shadow shadow-black text-white",
-            // }}
-          >
-            تگ خونه
-          </Chip>
-          <Chip
-            size="lg"
-            variant="shadow"
-            classNames={{
-              base: "bg-gradient-to-br from-indigo-500 to-pink-500 border-white/50 shadow-pink-500/30",
-              content: "drop-shadow shadow-black text-white",
-            }}
-          >
-            تگ آسانسور
-          </Chip>
-          <Chip
-            size="lg"
-            variant="shadow"
-            classNames={{
-              base: "bg-gradient-to-br from-indigo-500 to-pink-500  border-white/50 shadow-pink-500/30",
-              content: "drop-shadow shadow-black text-white",
-            }}
-          >
-            تگ پارکینگ
-          </Chip> */}
+            <img src={FiltersIcon} width={20} height={20} />
+          </div>
+
+          <AnimatePresence>
+            {inputValue && (
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                exit="hidden"
+                variants={buttonVariants}
+                className="absolute left-4 bottom-4"
+              >
+                <Button color="primary" radius="sm" size="lg">
+                  شروع جستجو
+                </Button>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </div>
-    </main>
+
+      <Modal
+        backdrop="blur"
+        isOpen={isOpen}
+        size="lg"
+        onOpenChange={onModalClose}
+        closeButton={<></>}
+        classNames={{
+          footer: "",
+        }}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              {category ? (
+                <ModalHeader className="flex items-center justify-between">
+                  <span className="text-xl font-bold">فیلترها</span>
+                  <div
+                    className="flex gap-2 text-gray-700 cursor-pointer mt-1"
+                    onClick={() => setCategory("")}
+                  >
+                    <span className="text-sm">
+                      انتخاب دسته‌بندی ({categoryName})
+                    </span>
+                    <span className="text-xl">←</span>
+                  </div>
+                </ModalHeader>
+              ) : (
+                ""
+              )}
+
+              <ModalBody className="py-6">
+                {category ? (
+                  category === "buy" ? (
+                    <BuyFilters />
+                  ) : (
+                    <RentFilters />
+                  )
+                ) : (
+                  <div className="w-full py-4">
+                    <div
+                      className="flex-1 h-20 bg-slate-100 flex items-center justify-center  hover:bg-gradient-to-r from-red-400 via-pink-600 to-purple-700 text-gray-900 hover:text-white cursor-pointer"
+                      onClick={() => setCategory("rent")}
+                    >
+                      <span className="text-xl font-black">رهن و اجاره</span>
+                    </div>
+                    <Divider className="bg-slate-200" />
+                    <div
+                      className="flex-1 h-20 bg-slate-100 flex items-center justify-center  hover:bg-gradient-to-r from-red-400 via-pink-600 to-purple-700 text-gray-900 hover:text-white cursor-pointer"
+                      onClick={() => setCategory("buy")}
+                    >
+                      <span className="text-xl font-black">خرید</span>
+                    </div>
+                  </div>
+                )}
+              </ModalBody>
+
+              {category && (
+                <ModalFooter>
+                  <Button
+                    size="lg"
+                    radius="sm"
+                    color="danger"
+                    onPress={onClose}
+                    className="flex-1"
+                  >
+                    انصراف
+                  </Button>
+                  <Button
+                    size="lg"
+                    radius="sm"
+                    color="primary"
+                    onPress={onClose}
+                    className="flex-1"
+                  >
+                    تایید
+                  </Button>
+                </ModalFooter>
+              )}
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </motion.main>
   );
 }
 
---
+++ src/components/BuyFIlters/index.jsx
@@ -0,0 +1,57 @@
+import { Input, Select, SelectItem, Slider } from "@nextui-org/react";
+
+const CITIES = [
+  { key: "teh", label: "تهران" },
+  { key: "shir", label: "شیراز" },
+  { key: "esph", label: "اصفهان" },
+];
+
+const BuyFilters = () => {
+  return (
+    <>
+      <div className="flex w-full flex-wrap md:flex-nowrap gap-3">
+        <Input
+          type="number"
+          label="حداقل قیمت"
+          endContent={<span className="text-xs font-black">﷼</span>}
+        />
+        <Input
+          type="number"
+          label="حداکثر قیمت"
+          endContent={<span className="text-xs font-black">﷼</span>}
+        />
+      </div>
+
+      <div className="mb-2">
+        <Select
+          className="w-full"
+          label="شهر را انتخاب کنید"
+          classNames={{ base: "max-w-full" }}
+        >
+          {CITIES.map((city) => (
+            <SelectItem key={city.key}>{city.label}</SelectItem>
+          ))}
+        </Select>
+      </div>
+
+      <div>
+        <Slider
+          label="متراژ"
+          step={10}
+          size="md"
+          color="foreground"
+          minValue={20}
+          maxValue={1000}
+          className="max-w-md"
+          defaultValue={[0, 500]}
+          classNames={{ value: "ltr" }}
+          formatOptions={{
+            style: "decimal",
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default BuyFilters;
